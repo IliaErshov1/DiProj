c5e50d88713800234d4459c034f4cb37743b4b57
IliaErshov1 <91780978+IliaErshov1@users.noreply.github.com>
2021-11-19 14:23:12 +0400

    добавлена работа с json
    func workJSON()

--- main.go
+++ main.go
@@ -2,16 +2,33 @@ package main
 
 import ("fmt"
         "net/http"
-        "html/template")
+        "html/template"
+        "encoding/json"
+        "io/ioutil"
+        "log"
+        "time")
 
-type User struct{
-  Name string
-  Age uint16
+type Music struct{
+  Kind string `json:"Kind"`
+  CollectionName string `json:"CollectionName"`
+  TrackName string `json:"TrackName"`
+  CollectionPrice float32 `json:"CollectionPrice"`
+  TrackPrice float32 `json:"TrackPrice"`
+  PrimaryGenreName string `json:"PrimaryGenreName"`
+  TrackCount int `json:"TrackCount"`
+  TrackNumber int `json:"TrackNumber"`
+  ReleaseDate string `json:"ReleaseDate"`
+  // Name string
+  // Age uint16
 //  Money int16
 //  Avg_grades, Happiness float64
 //  Hobbis []string
 }
 
+type Results struct {
+    ResultCount  int `json:"ResultCount"`
+    Results []Music `json:"Results"`
+}
 // func(u User) getAllInfo() string {
 //   return fmt.Sprintf("User name %s. He is %  money %d", u.Name, u.Age, u.Money)
 // }
@@ -22,18 +39,17 @@ type User struct{
 
 func home_page(w http.ResponseWriter, r *http.Request){
   //bob := User{"Bob", 25}
-  bob := []User{
-    {Name: "Columbia Memorial Station", Age: 5,},
-    {Name: "Challenger Memorial Station", Age: 7},
-    {Name: "Cggg", Age: 8},
+  musics := []Music{
+    {Kind: "Columbia Memorial Station", CollectionPrice: 5,},
+    {Kind: "Challenger Memorial Station", CollectionPrice: 7},
+    {Kind: "Cggg", CollectionPrice: 8},
 }
 
 r.ParseForm()
-    // они все тут
-//  params := r.Form
 params := r.FormValue("foo")
 switch params {
     case "download":
+      workJSON()
  fmt.Println(params)
 
     case "remove":
@@ -41,7 +57,6 @@ switch params {
 
       case "select":
    fmt.Println(params)
-
  default:
      fmt.Println(params)
   }
@@ -56,7 +71,7 @@ switch params {
 //  fmt.Fprintf(w, "Test web" + bob.getAllInfo())
 //fmt.Fprintf(w, <b>Main Trxt</b>")
 tmpl, _ := template.ParseFiles("templates/home_page.html")
-tmpl.Execute(w, bob)
+tmpl.Execute(w, musics)
 }
 
 func hadleRequest(){
@@ -80,4 +95,53 @@ func main(){
 
 func testb(){
   fmt.Println("Connect Done")
+}
+
+func workJSON() {
+    url := "https://itunes.apple.com/search?term=The+Beatles"
+
+    var netClient = http.Client{
+        Timeout: time.Second * 10,
+    }
+
+    res, err := netClient.Get(url)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer res.Body.Close()
+
+    body, err := ioutil.ReadAll(res.Body)
+
+  //  fmt.Println(body)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    astros := Results{}
+
+    jsonErr := json.Unmarshal(body, &astros)
+	 //jsonErr := json.MarshalIndent(&astros, "", "    ")
+	// fmt.Println(&astros)
+
+    if jsonErr != nil {
+        log.Fatal(jsonErr)
+    }
+
+
+			for i := 0; i < astros.ResultCount; i++{
+	  fmt.Println(i)
+		fmt.Println(astros.Results[i].Kind)
+		fmt.Println(astros.Results[i].CollectionName)
+		fmt.Println(astros.Results[i].TrackName)
+		fmt.Println(astros.Results[i].CollectionPrice)
+		fmt.Println(astros.Results[i].TrackPrice)
+		fmt.Println(astros.Results[i].PrimaryGenreName)
+		fmt.Println(astros.Results[i].TrackCount)
+		fmt.Println(astros.Results[i].TrackNumber)
+		fmt.Println(astros.Results[i].ReleaseDate)
+		fmt.Println("______________")
+			}
 }
